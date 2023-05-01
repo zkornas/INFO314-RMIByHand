0e8295afab95883fd9ddcc392490c5d574e281a8
zkornas <zkornas@uw.edu>
2023-05-01 15:34:22 -0700

    pickle + addition functional

--- Python/Client.py
+++ Python/Client.py
@@ -1,11 +1,66 @@
+import socket
+import logging
+import pickle
+
+host = ""
+port = 10314
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+logging.info("Connecting to service: {} on port (${})".format(host, port))
+
+class addition:
+    ID = "add"
+    lhs = 0
+    rhs = 0
+    ans = 0
+
+class division:
+    ID = "divide"
+    lhs = 0
+    rhs = 0
+    ans = 0
+
+class repeater:
+    ID = "echo"
+    msg = ""
+    response = ""
+
+
 def add(lhs, rhs):
-    return -1
+    add_object = addition()
+    add_object.lhs = lhs
+    add_object.rhs = rhs
+
+    add_pickle = pickle.dumps(add_object)
+
+    s.send(add_pickle)
+    logging.info("Sending pickle to server")
+
+    data = s.recv(1024)
+    logging.info("Receiving data from server")
+
+    data_unpickled = pickle.loads(data)
+    return data_unpickled.ans
 
 def divide(lhs, rhs):
     return -1
 
 def echo(msg):
-    return -1
+    echo_object = repeater()
+    echo_object.msg = msg
+
+    echo_pickle = pickle.dumps(echo_object)
+
+    s.send(echo_pickle)
+    print("Sending echo pickle to server")
+
+    data = s.recv(1024)
+    print("Receiving echo pickle from server")
+
+    data_unpickled = pickle.loads(data)
+    return data_unpickled.response
+
 
 # --------------------------------------------
 # The following exercises the remote calls
@@ -17,12 +72,11 @@ if (add(2, 4) == 6):
 else:
     print("X")
 
-try:
-    divide(1, 0)
-    print("X")
-except Exception:
-    print(".")
-
+#try:
+#    divide(1, 0)
+#    print("X")
+#except Exception:
+#    print(".")
 
 if echo("Hello") == "You said Hello!":
     print(".")
--- Python/Server.py
+++ Python/Server.py
@@ -1,5 +1,23 @@
 import sys
 import socket
+import pickle
+
+class addition:
+    ID = "add"
+    lhs = 0
+    rhs = 0
+    ans = 0
+
+class division:
+    ID = "divide"
+    lhs = 0
+    rhs = 0
+    ans = 0
+
+class repeater:
+    ID = "echo"
+    msg = ""
+    response = ""
 
 # Do not modify the add() function
 def add(lhs, rhs):
@@ -22,15 +40,37 @@ def main():
     port = 10314
 
     # Open Socket
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    tcp_socket.bind((host, port))
-    tcp_socket.listen()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host, port))
+    s.listen()
+
+    print("listening")
 
     while True:
-        client_connection, client_address = tcp_socket.accept()
-        
-        request = client_connection.recv(1024)
+        client_connection, client_address = s.accept()
+
+        data = client_connection.recv(1024)
+        data_unpickled = pickle.loads(data)
+        print("Getting pickle!")
+
+        if data_unpickled.ID == "add":
+            print("We have addition!")
+            data_unpickled.ans = add(data_unpickled.lhs, data_unpickled.rhs)
+            data_pickle = pickle.dumps(data_unpickled)
+            client_connection.send(data_pickle)
+        #elif data_unpickled.ID == "divide":
+        #    data_unpickled.ans = divide(data_unpickled.lhs, data_unpickled.rhs)
+        #    data_pickle = pickle.dumps(data_unpickled)
+        #    client_connection.send(data_pickle)
+        elif data_unpickled.ID == "echo":
+            print("We have an echo!")
+            data_unpickled.response == echo(data_unpickled.msg)
+            data_pickle = pickle.dumps(data_unpickled)
+            client_connection.send(data_pickle)
+            print("sending back pickled echo")
+
+        client_connection.close()
     pass
 
 if __name__ == '__main__':
